4151573b4f2c7f28f9fa9cba6b1f957c91a61b8a
vagrant <ahbabousara@gmail.com>
2016-08-11 03:35:59 +0000

    Added user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Petsitter::Application.routes.draw do
     resources :reviews, only: :create
   end
 
+  resources :users, only: :show
+  
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
